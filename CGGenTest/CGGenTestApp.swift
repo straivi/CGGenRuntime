56b4998a6d98a8dbc07bba5cde82b3fffc16998a
Matvei Borisov <bme15122000@gmail.com>
2023-10-31 17:52:12 +0300

    Initial Commit

---
+++ CGGenTest/CGGenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CGGenTestApp.swift
+//  CGGenTest
+//
+//  Created by Матвей Борисов on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CGGenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
